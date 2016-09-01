28ef5af3148c7e65c5c5b70c669256bb30c230d4
Your Name <you@example.com>
2016-09-01 22:59:33 +0300

    updated script

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -805,6 +805,22 @@ References:
 	var jsonUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/pv.json';
 	
 	var currentArea, hitPoints, stageName, stageType, stageMoves, teamLimit, recommendedParty, srankStrat, captureRate, clearStrat, disruptions, disruptionBoard, disruptionInit, disruptionTimer;
+
+	// URL for resources
+	var jsonUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/stageGuides/cn.json';
+	var stageTypesUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/stageTypes.json';
+
+	// setup reference to stage types
+	var stageTypes;
+
+	//
+	var currentArea, hitPoints, stageName, stageType, stageMoves, 
+			teamLimit, recommendedParty, srankStrat, clearStrat, disruptions,
+			basePower, ability, captureRate;
+
+	$.getJSON(stageTypes, function (data) {
+		stageTypesUrl = data;
+	});
 	
 	var stageData = $('body').attr('stage-data');
 	console.log('searched stage is ' + stageData);
@@ -822,15 +838,16 @@ References:
 				stageMoves = item['moves'];
 				teamLimit = item['pokemon'];
 				recommendedParty = item['recommendedParty'];
-				captureRate = item['captureRate'];
+				captureRate = splitBreakLine(item['captureRate']);
 				srankStrat = item['srankingStrategy'];
 				clearStrat = item['clearingStrategy'];
 				disruptions = item['disruptions'];
+				basePower = item['basePower'];
+				ability = item['ability'];
 				
 				$('.stage__thumbnail').attr('src', 'images/icons/icon_' + stageData + '.png');
 				$('span.stage-type').text(stageType);
 				$('span.stage-hp').text(hitPoints);
-				$('span.stage-capture').text(captureRate);
 				$('span.stage-limit').text(teamLimit);
 				$('span.stage-moves').text(stageMoves);
 				$('.stage-name').text(stageName);
@@ -855,10 +872,29 @@ References:
 					});
 				}
 				
-				console.log('board: ' + disruptionBoard);
-				console.log('init: ' + disruptionInit);
-				console.log('init: ' + disruptionTimer);
+				// handle capture rate
+				handleCaptureRate(captureRate);
+				$('.stage-power').text(basePower);
+				$('.stage-ability').text(ability);
 			}	
 		});
 	});
+
+	var splitBreakLine = function (data) {
+		return data.split('\n');
+	}
+
+	var handleCaptureRate = function (captureRate) {
+		if (captureRate.length < 2) {
+			$('.stage-capture').text('N/A');
+			$('.stage-capture-add').text('N/A');	
+		} else {
+			var captureRateInit = parseInt(captureRate[0].slice(5, -1));
+			$('.stage-capture').text(captureRateInit);
+			var captureRateBonus = parseInt(captureRate[1].slice(6, -1));
+			$('.stage-capture-bonus').text(captureRateBonus);
+		}
+	};
+
+	var handle
 })();
