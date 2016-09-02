eaa893cba1b7e3b10b0e4adac527f9d51ccaccf2
shelune <hieu@mios.fi>
2016-09-02 14:26:28 +0300

    updated script for more data fetching

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -1,19 +1,18 @@
 ;(function () {
 	var jsonUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/pv.json';
-	
-	var currentArea, hitPoints, stageName, stageType, stageMoves, teamLimit, recommendedParty, srankStrat, captureRate, clearStrat, disruptions, disruptionBoard, disruptionInit, disruptionTimer;
 
 	// URL for resources
-	var jsonUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/stageGuides/cn.json';
+	var jsonUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/stageGuides/rc.json';
 	var stageTypesUrl = 'https://rawgit.com/shelune/poke-shuffle-guide/master/app/scripts/assets/stageTypes.json';
 
 	// setup reference to stage types
 	var stageTypes;
 
 	//
-	var currentArea, hitPoints, stageName, stageType, stageMoves, 
-			teamLimit, recommendedParty, srankStrat, clearStrat, disruptions,
-			basePower, ability, captureRate;
+	var currentArea, stageIcon, hitPoints, stageName, stageType, stageMoves, 
+			boardLayout, disruptions, disruptionInit, disruptionBoard, disruptionTimer,
+			basePower, ability, captureRate,
+			recommendedParty, srankStrat, clearStrat, teamLimit;
 
 	$.getJSON(stageTypes, function (data) {
 		stageTypesUrl = data;
@@ -29,10 +28,12 @@
 		data.map(function (item) {
 			
 			if (item['stageNo'].toString() === stageData) {
+				stageIcon = item['icon'];
 				hitPoints = item['hitPts'];
 				stageName = item['name'];
 				stageType = item['type'];
 				stageMoves = item['moves'];
+				boardLayout = item['initialBoardSetup'];
 				teamLimit = item['pokemon'];
 				recommendedParty = item['recommendedParty'];
 				captureRate = splitBreakLine(item['captureRate']);
@@ -42,7 +43,6 @@
 				basePower = item['basePower'];
 				ability = item['ability'];
 				
-				$('.stage__thumbnail').attr('src', 'images/icons/icon_' + stageData + '.png');
 				$('span.stage-type').text(stageType);
 				$('span.stage-hp').text(hitPoints);
 				$('span.stage-limit').text(teamLimit);
@@ -61,22 +61,30 @@
 					$.each(disruptionArr, function(line, value) {
 						if (value.includes('Board:')) {
 							disruptionBoard = value.slice(6, value.length);
-						} else if (value.includes('Initial:')) {
+						} else if (value.includes('Initial')) {
 							disruptionInit = value.slice(8, value.length);
-						} else if (value.includes('Timer: ')) {
+						} else if (value.includes('Timer')) {
 							disruptionTimer = value.slice(6, value.length);
 						}
 					});
 				}
-				
-				console.log('board: ' + disruptionBoard);
-				console.log('init: ' + disruptionInit);
-				console.log('init: ' + disruptionTimer);
+
+				// handle stage icon
+				handleStageIcon(stageIcon);
 
 				// handle capture rate
 				handleCaptureRate(captureRate);
 				$('.stage-power').text(basePower);
 				$('.stage-ability').text(ability);
+
+				// handle board layout
+				handleStageLayout(boardLayout);
+
+				// handle clearing strategy
+				handleStageClearing(clearStrat);
+
+				// handle srank strategy
+				handleStageSRank(srankStrat);
 			}	
 		});
 	});
@@ -96,5 +104,29 @@
 			$('.stage-capture-bonus').text(captureRateBonus);
 		}
 	};
+
+	var handleStageIcon = function(stageIcon) {
+		$('.stage__thumbnail').attr('src', 'images/icons/icon_' + stageIcon + '.png');
+	}
+
+	var handleStageLayout = function(stageLayoutUrl) {
+		$('.setup__layout').attr('src', stageLayoutUrl);
+	}
+
+	var handleStageClearing = function(stageClearing) {
+		$('.strategy__walkthrough-content.clearing').text(stageClearing);
+	}
+
+	var handleStageSRank = function(stageSRank) {
+		var movesInitPos = stageSRank.indexOf('least');
+		var movesLastPos = stageSRank.indexOf('left');
+		var movesSRank = stageSRank.slice(movesInitPos + 6, movesLastPos - 6);
+
+		var strat = stageSRank.split('\n');
+		strat.shift();
+		
+		$('.stage-srank-moves').text(movesSRank);
+		$('.strategy__walkthrough-content.srank').text(strat.join(' '));
+	}
 	
 })();
